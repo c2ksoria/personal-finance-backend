757be358dd728ef3827028cdb5e620da90eacfd9
Carlos Valdemar Soria <carlosvsoria1982@gmail.com>
2024-03-21 17:11:13 -0300

    some new changes on .env constants

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,10 +1,10 @@
 //configure global enviroment
 const PORTAPP = process.env.PORT || 3001
-const DB_HOST = process.env.RAILWAY_TCP_PROXY_DOMAIN || 'localhost'
-const DB_USER = process.env.MYSQLUSER || 'root'
-const DB_PASSWORD = process.env.MYSQL_ROOT_PASSWORD || 'rootpass'
-const DB_NAME = process.env.MYSQL_DATABASE || 'finance'
-const DB_PORT = process.env.RAILWAY_TCP_PROXY_PORT || 3306
+const DB_HOST = process.env.MySQL.MYSQLHOST || 'localhost'
+const DB_USER = process.env.MySQL.MYSQLUSER || 'root'
+const DB_PASSWORD = process.env.MySQL.MYSQL_ROOT_PASSWORD || 'rootpass'
+const DB_NAME = process.env.MySQL.MYSQL_DATABASE || 'finance'
+const DB_PORT = process.env.MySQL.MYSQLPORT || 3306
 
 const config = {
     appConfig:
