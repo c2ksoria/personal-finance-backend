00fc96472a4efd0c62cace583a602bef0d0c0a44
Carlos Valdemar Soria <carlosvsoria1982@gmail.com>
2024-03-21 17:21:41 -0300

    new .env constanst are created

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,10 +1,10 @@
 //configure global enviroment
 const PORTAPP = process.env.PORT || 3001
-const DB_HOST = process.env.MySQL.MYSQLHOST || 'localhost'
-const DB_USER = process.env.MySQL.MYSQLUSER || 'root'
-const DB_PASSWORD = process.env.MySQL.MYSQL_ROOT_PASSWORD || 'rootpass'
-const DB_NAME = process.env.MySQL.MYSQL_DATABASE || 'finance'
-const DB_PORT = process.env.MySQL.MYSQLPORT || 3306
+const DB_HOST = process.env.DB_HOST || 'localhost'
+const DB_USER = process.env.DB_USER || 'root'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'rootpass'
+const DB_NAME = process.env.DB_NAME || 'finance'
+const DB_PORT = process.env.DB_PORT || 3306
 
 const config = {
     appConfig:
